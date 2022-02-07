9d1d2af027b9c8451bec5b71a8f1bfef7c6a8ed2
0xTheOldOne <oxtheoldone@protonmail.com>
2022-02-07 20:31:11 +0100

    About page relooking

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,123 +2,130 @@
   <b-container>
     <b-row>
       <b-col>
-        <p>
-          <b-badge variant="warning" class="mr-2">
-            <b-icon icon="exclamation-triangle" class="mr-1" />Disclaimer<b-icon icon="exclamation-triangle" class="ml-2" />
-          </b-badge>
-          This tool was <u>not</u> created in order to compete with <a href="https://twitter.com/phtevenstrong" target="_blank" rel="noopener noreferrer">The Calculator Guy</a>'s existant calculator.
-        </p>
-        <p>
-          When i wrote this app i was just starting to earn/compound strong tokens in order to create strong nodes and i tought that it could be cool to have some earnings calculator, especially if you're new to StrongBlock because this can help if you don't know how it works.
-        </p>
-        <p>
-          Also, i thought that it could be cool if my app was able to connect to a wallet and see how many tokens you have in your wallet, how many nodes too or how many earnings you had but i <b>don't</b> know how to do that at the moment and i <b>don't</b> want this app to be able to interact with people's wallets because nowadays we see a lot of scammers trying to do so and people could be in fear about that and that's not what i want, stay safe ! This app will not retrieve those kind of informations then, i hope it's ok for you !
-        </p>
-        <p>
-          Please keep in mind that it was originally created in order to practice VueJs basics so maybe it's not perfect...
-        </p>
-        <p>
-          Last thing about all of this is i code on my free time (<i>i have a full-time job and a new born, so i don't have a lot of free time unfortunatly</i>) so this tool is provided "as is" and i'll update it when i have enough time.
-        </p>
-        <p>Thank you for your understanding and many thanks for using Strong Calculator !</p>
+        <b-card class="mb-4">
+          <b-card-text>
+            <p>
+              <b-badge variant="warning" class="mr-2">
+                <b-icon icon="exclamation-triangle" class="mr-1" />Disclaimer<b-icon icon="exclamation-triangle" class="ml-2" />
+              </b-badge>
+              This tool was <u>not</u> created in order to compete with <a href="https://twitter.com/phtevenstrong" target="_blank" rel="noopener noreferrer">The Calculator Guy</a>'s existant calculator.
+            </p>
+            <p>
+              When i wrote this app i was just starting to earn/compound strong tokens in order to create strong nodes and i tought that it could be cool to have some earnings calculator, especially if you're new to StrongBlock because this can help if you don't know how it works.
+            </p>
+            <p>
+              Also, i thought that it could be cool if my app was able to connect to a wallet and see how many tokens you have in your wallet, how many nodes too or how many earnings you had but i <b>don't</b> know how to do that at the moment and i <b>don't</b> want this app to be able to interact with people's wallets because nowadays we see a lot of scammers trying to do so and people could be in fear about that and that's not what i want, stay safe ! This app will not retrieve those kind of informations then, i hope it's ok for you !
+            </p>
+            <p>
+              Please keep in mind that it was originally created in order to practice VueJs basics so maybe it's not perfect...
+            </p>
+            <p>
+              Last thing about all of this is i code on my free time (<i>i have a full-time job and a new born, so i don't have a lot of free time unfortunatly</i>) so this tool is provided "as is" and i'll update it when i have enough time.
+            </p>
+            <p>Thank you for your understanding and many thanks for using Strong Calculator !</p>
+          </b-card-text>
+        </b-card>
       </b-col>
     </b-row>
-    <b-row class="mb-3">
-      <b-col sm="8" xs="12" class="mt-5">
-        <u>Technical informations :</u>
-        <p class="mb-3">
-          <ul class="icons mt-2">
-            <li>
-              <b-icon icon="chevron-right" />
-              The app layout is made with <a href="https://bootstrap-vue.org/" target="_blank" rel="noopener noreferrer">bootstrap-vue</a>
-            </li>
-            <li>
-              <b-icon icon="chevron-right" />
-              Charts are made with <a href="https://www.highcharts.com/" target="_blank" rel="noopener noreferrer">Highcharts</a>
-            </li>
-            <li>
-              <b-icon icon="chevron-right" />
-              Strong token price in USD is fetched using <a href="https://github.com/miscavage/CoinGecko-API" target="_blank" rel="noopener noreferrer">Miscavage's wrapper</a> on <a href="https://www.coingecko.com/api/docs/v3" target="_blank" rel="noopener noreferrer">CoinGecko API</a>
-            </li>
-            <li>
-              <b-icon icon="chevron-right" />
-              Github avatar and bio are fetched using <a href="https://github.com/axios/axios" target="_blank" rel="noopener noreferrer">Axios</a> and <a href="https://docs.github.com/en/rest/reference/users" target="_blank" rel="noopener noreferrer">GitHub Rest API</a>
-            </li>
-          </ul>
-        </p>
-
-        <u>To do, ordered by priority :</u>
-        <p class="mb-3">
-          <ul class="icons mt-2">
-             <li>
-              <b-icon icon="check-circle" variant="success" class="mr-1" />
-              Add token price chart
-            </li>
-            <li>
-            <b-icon icon="check-circle" variant="success" class="mr-1" />
-              Store values with <a href="https://vuex.vuejs.org/" target="_blank" rel="noopener noreferrer">VueX</a> store in order to use those values across the app
-            </li>
-            <li>
-              <b-icon icon="circle" class="mr-1" />
-              Refactor rewards component in order to use VueX store values
-            </li>
-            <li>
-              <b-icon icon="question-circle" class="mr-1" />
-              Maybe add some internationalization (i18n) for non english speakers
-            </li>
-            <li>
-              <b-icon icon="circle" class="mr-1" />
-              Add a projection chart of earnings
-            </li>
-            <li>
-              <b-icon icon="circle" class="mr-1" />
-              Add an auto-compound option to this chart so you can see when you will be able to create the next node
-            </li>
-            <li>
-              <b-icon icon="circle" class="mr-1" />
-              Add monthly fees to the earnings projections (depends on the visualization i choose, it could be a mess with a lot of nodes)
-            </li>
-            <li>
-              <b-icon icon="question-circle" class="mr-1" />
-              Maybe add NFT tab
-            </li>
-            <li>
-              <b-icon icon="question-circle" class="mr-1" />
-              Maybe add Polygon nodes tab
-            </li>
-            <li>
-              <b-icon icon="question-circle" class="mr-1" />
-              Maybe add Sentinel nodes tab
-            </li>
-            <li>
-              <b-icon icon="question-circle" class="mr-1" />
-              Maybe add an ETH gas tracker
-            </li>
-          </ul>
-        </p>
-        <p>
-          <b-badge variant="primary" class="mr-2">Nota bene</b-badge>
-          I don't own NFTs or <img src="polygon.png" class="logo" />Polygon nodes or <img src="sentinel.png" class="logo" />Sentinel nodes at the moment so i don't know how they work, yet...
-        </p>
+    <b-row class="mb-4">
+      <b-col sm="7" xs="12">
+        <b-card class="mb-4">
+          <b-card-text>
+            <u>Technical informations :</u>
+            <p class="mb-3">
+              <ul class="icons mt-2">
+                <li>
+                  <b-icon icon="chevron-right" />
+                  The app layout is made with <a href="https://bootstrap-vue.org/" target="_blank" rel="noopener noreferrer">bootstrap-vue</a>
+                </li>
+                <li>
+                  <b-icon icon="chevron-right" />
+                  Charts are made with <a href="https://www.highcharts.com/" target="_blank" rel="noopener noreferrer">Highcharts</a>
+                </li>
+                <li>
+                  <b-icon icon="chevron-right" />
+                  Strong token price in USD is fetched using <a href="https://github.com/miscavage/CoinGecko-API" target="_blank" rel="noopener noreferrer">Miscavage's wrapper</a> on <a href="https://www.coingecko.com/api/docs/v3" target="_blank" rel="noopener noreferrer">CoinGecko API</a>
+                </li>
+                <li>
+                  <b-icon icon="chevron-right" />
+                  Github avatar and bio are fetched using <a href="https://github.com/axios/axios" target="_blank" rel="noopener noreferrer">Axios</a> and <a href="https://docs.github.com/en/rest/reference/users" target="_blank" rel="noopener noreferrer">GitHub Rest API</a>
+                </li>
+              </ul>
+            </p>
+          </b-card-text>
+        </b-card>
+        <b-card class="mb-4">
+          <b-card-text>
+            <u>To do, ordered by priority :</u>
+            <p class="mb-3">
+              <ul class="icons mt-2">
+                <li>
+                  <b-icon icon="check-circle" variant="success" class="mr-1" />
+                  Add token price chart
+                </li>
+                <li>
+                <b-icon icon="check-circle" variant="success" class="mr-1" />
+                  Store values with <a href="https://vuex.vuejs.org/" target="_blank" rel="noopener noreferrer">VueX</a> store in order to use those values across the app
+                </li>
+                <li>
+                  <b-icon icon="circle" class="mr-1" />
+                  Refactor rewards component in order to use VueX store values
+                </li>
+                <li>
+                  <b-icon icon="question-circle" class="mr-1" />
+                  Maybe add some internationalization (i18n) for non english speakers
+                </li>
+                <li>
+                  <b-icon icon="circle" class="mr-1" />
+                  Add a projection chart of earnings
+                </li>
+                <li>
+                  <b-icon icon="circle" class="mr-1" />
+                  Add an auto-compound option to this chart so you can see when you will be able to create the next node
+                </li>
+                <li>
+                  <b-icon icon="circle" class="mr-1" />
+                  Add monthly fees to the earnings projections (depends on the visualization i choose, it could be a mess with a lot of nodes)
+                </li>
+                <li>
+                  <b-icon icon="question-circle" class="mr-1" />
+                  Maybe add NFT tab
+                </li>
+                <li>
+                  <b-icon icon="question-circle" class="mr-1" />
+                  Maybe add Polygon nodes tab
+                </li>
+                <li>
+                  <b-icon icon="question-circle" class="mr-1" />
+                  Maybe add Sentinel nodes tab
+                </li>
+                <li>
+                  <b-icon icon="question-circle" class="mr-1" />
+                  Maybe add an ETH gas tracker
+                </li>
+              </ul>
+            </p>
+            <p>
+              <b-badge variant="primary" class="mr-2">Nota bene</b-badge>
+              I don't own NFTs or <img src="polygon.png" class="logo" />Polygon nodes or <img src="sentinel.png" class="logo" />Sentinel nodes at the moment so i don't know how they work, yet...
+            </p>
+          </b-card-text>
+        </b-card>
       </b-col>
-      <b-col sm="4" xs="12" class="mt-5">
-        <u>Details about the {{githubHistoryDepth}} last source updates :</u>
-        <b-overlay :show="!loaded" variant="transparent" opacity="0.8" blur="5px" rounded="sm">
-          <div v-if="loaded" class="commits mt-2">
-            <div class="commit mb-1" v-for="commit in githubResponse" :key="commit.sha">
-              <div class="message">{{commit.commit.message}}</div>
-              <div class="infos">
-                <div class="sha">
-                  {{commit.sha.substring(0,7)}}
-                </div>
-                <div class="date">
-                  {{new Date(commit.commit.author.date).toLocaleString()}}
+      <b-col sm="5" xs="12">
+        <b-card class="mb-3">
+          <b-card-text>
+            <u>Details about the {{githubHistoryDepth}} last updates :</u>
+            <b-overlay :show="!loaded" variant="transparent" opacity="0.8" blur="5px" rounded="sm">
+              <div v-if="loaded" class="commits mt-2">
+                <div class="commit mb-2" v-for="commit in githubResponse" :key="commit.sha">
+                    <div class="message">{{commit.commit.message}}</div>
+                    <div class="date"><b-badge class="sha mr-2">{{commit.sha.substring(0,7)}}</b-badge>{{new Date(commit.commit.author.date).toLocaleString()}}</div>
                 </div>
               </div>
-              
-            </div>
-          </div>
-        </b-overlay>
+            </b-overlay>
+          </b-card-text>
+        </b-card>
       </b-col>
     </b-row>
   </b-container>
@@ -167,4 +174,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 @import "../assets/style/variables.less";
+
+.commit {
+  font-size: .9rem;
+
+  .sha {
+    font-family: "Source Code Pro", monospace;
+  }
+}
 </style>
