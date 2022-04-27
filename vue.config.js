1d0a0778e56b630f16475cf15d56410fa65904c1
0xTheOldOne <oxtheoldone@protonmail.com>
2022-04-27 19:56:09 +0200

    deploy test

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/",
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
       enableInSFC: false,
       includeLocales: false,
-      enableBridge: true
-    }
-  }
-}
+      enableBridge: true,
+    },
+  },
+};
